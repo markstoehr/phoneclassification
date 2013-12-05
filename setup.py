2e33171e1bf21554f3283126ba2489f4c47faa61
Mark Stoehr <stoehr.mark@gmail.com>
2013-12-05 13:08:07 -0600

    setup file for compilation of the cython code

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    name = "Time Frequency Features for phone classification",
+    ext_modules = cythonize(['src/_tapers.pyx','src/_reassignment.pyx','src/hog_gray_scale.py']), # accepts a glob pattern
+)
