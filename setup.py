af481be1a21df05b60a9d391dff1190a4380fa71
Mark Stoehr <stoehr.mark@gmail.com>
2013-12-19 12:53:16 -0600

    added in compile information for ther bernoullimm.pyx file

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 
 setup(
     name = "Time Frequency Features for phone classification",
-    ext_modules = cythonize(['src/_tapers.pyx','src/_reassignment.pyx','src/hog_gray_scale.pyx']), # accepts a glob pattern
+    ext_modules = cythonize(['src/_tapers.pyx','src/_reassignment.pyx','src/hog_gray_scale.pyx','src/_bernoullimm.pyx']), # accepts a glob pattern
 )
