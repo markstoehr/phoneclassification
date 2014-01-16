3b2c86d9f563df3883d51c037743941da7bd5d3e
markstoehr <stoehr.mark@gmail.com>
2014-01-16 12:19:23 -0600

    small changes to prevent errors on a different platform

--- local/bmmhierarchy.py
+++ local/bmmhierarchy.py
@@ -2,7 +2,9 @@ from __future__ import division
 from src import bernoullimm
 import numpy as np
 
-rootdir='/home/mark/Research/phoneclassification'
+# rootdir='/home/mark/Research/phoneclassification'
+rootdir='/var/tmp/stoehr/phoneclassification'
+
 confdir='%s/conf'%rootdir
 datadir='%s/data/local/data' % rootdir
 expdir='%s/exp/bmm_hierarchy' % rootdir
@@ -42,36 +44,36 @@ bmm.weights_ = weights_
 bmm.fitthresholdlabels(X,y39,threshold=75)
 
 np.save('%s/layer1_class_means_9c.npy' % expdir,bmm.means_)
-np.save('%s/layer1_class_weights_9.npy' % expdir,bmm.weights_)
+np.save('%s/layer1_class_weights_9c.npy' % expdir,bmm.weights_)
 class_clusters = bmm.cluster_underlying_data(class_ids,X)
 np.save('%s/layer1_class_clusters_9c.npy' % expdir,class_clusters)
 responsibilities=bmm.predict_proba(X)
-np.save('%s/layer1_class_responsibilities_9c.npy' % expdir,responsibilities.T)
+np.save('%s/layer1_class_responsibilities_9c.npy' % expdir,responsibilities)
 
+
+responsibilities = np.load('%s/layer1_class_responsibilities_9c.npy' % expdir)
 n_components=9
 phoneclass_bclass_counts = np.zeros((len(leehon),n_components),dtype=int)
-
+classifier_tree = np.zeros(phoneclass_bclass_counts.shape,dtype=np.uint8)
 for pclass_id in xrange(len(leehon)):
     phoneclass_bclass_counts[pclass_id] = responsibilities[y==pclass_id].sum(0)
+    classifier_tree[pclass_id] = ((phoneclass_bclass_counts[pclass_id]/30).astype(int) > 0 ).astype(np.uint8)
 
-
+np.save('%s/layer1_classifier_tree9c.npy' % expdir,classifier_tree.T)
+    
 for phone_id, phone in enumerate(leehon[:,0]):
     print phone_id
-    for component_id in n_components:
+    for component_id in xrange(n_components):
         print component_id
         layer_n_components = min(int(phoneclass_bclass_counts[phone_id,component_id]/30),6)
-
         if layer_n_components == 0: continue
-
-        bmm=bernoullimm.BernoulliMM(n_components=9,n_iter=100,n_init=5,verbose=True,float_type=np.float32,init_params='mw')
-
+        bmm=bernoullimm.BernoulliMM(n_components=layer_n_components,n_iter=100,n_init=5,verbose=True,float_type=np.float32,init_params='mw')
         bmm.fit(X[
             (responsibilities[:,component_id] > .9)
             *(y==phone_id)])
+        np.save('%s/layer2_%s_%d_%d_means.npy' % (expdir,phone,phone_id,component_id),bmm.means_)
+        np.save('%s/layer2_%s_%d_%d_weights.npy' % (expdir,phone,phone_id,component_id),bmm.weights_)
 
-        y[curobs:curobs+nobs[phone_id]] = phone_id
-    y39[curobs:curobs+nobs[phone_id]] = leehon_dict[phone_id]
-    curobs += nobs[phone_id]
 
 class_ids = np.zeros((y.shape[0],len(leehon[:,0])),dtype=np.uint8)
 class_ids[np.arange(len(y)),y] = 1
@@ -127,7 +129,7 @@ KLdivs_sparse=np.exp(-KLdivs)
 threshold=.6
 KLdivs_sparse[KLdivs_sparse < threshold] = 0
 G = nx.Graph(KLdivs_sparse)
-rcm = list(reverse_cuthill_mckee_ordering(G))    
+rcm = list(reverse_cuthill_mckee_ordering(G))
 
 title='KL D(p||q) map 9 classes'
 figsize=6
