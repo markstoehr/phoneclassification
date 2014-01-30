49cf795d8db67fed26675fbc3bb1a56e40ef3f7c
Mark Stoehr <stoehr.mark@gmail.com>
2014-01-30 17:57:34 -0600

    added a multiclass pegasos without the mini-batch--has the correct update structure

--- phoneclassification/pegasos.py
+++ phoneclassification/pegasos.py
@@ -36,9 +36,161 @@ def perform_batch_updates(YX,T,l,k,w_init=None,v=False):
 
     return w
 
-def multiclass(Y,X,T,l,k,W_init,W_classes,start_t=100,v=False,loss='hinge',
+def multiclass_minibatch(Y,X,T,l,W,start_t=1,loss_computation=0,
+               return_avg_W=True,verbose=False,loss='hinge',
+               do_projection=False):
+    """
+    A multiclass implementation of the Pegasos
+    similar to the one presented by Zhuang Wang 2010
+
+    Parameters
+    ----------
+    Y :   (n_samples,)
+        Y classes
+    X :   (n_samples, n_features)
+        Data 
+    T :  int
+       Number of rounds over which to perform
+       optimization
+
+    l : float
+       lambda regularization parameter
+
+    W : (n_classifiers,n_features) 
+       Initializaiton for W -- required
+       the assumption is that W[c] is the
+       classifier for class c -- i.e. we work
+       from the rows as classifiers
+
+    return_avg_W : bool
+       If True then we return the W produced as the average
+       of the updates
+
+    start_t : int
+       Starting round -- in the case of a warm-start one
+       may not wish to begin with round 1 since, presumably
+       the first few rounds have already been passed
+
+    loss_computation : int
+       If 0 then we do not compute the loss over the data set while
+       running the algorithm (which is good if the dataset is large), 
+       otherwise if loss_computation > 0 we compute it every 
+       loss_computation rounds.
+
+    In each round we get the scores
+    then we construct a class-mask over the scores
+    and we map all the scores to a lower quantity
+    """
+    k = min(X.shape[0],k)
+    use_example_ids = np.random.randint(0,X.shape[0],size=(T,))
+    n_classes, n_features = W.shape
+    # class masks makes finding best non-class happen more easily
+    class_masks = np.eye(n_classes)
+    init_W = W.copy()
+    avg_W_update = np.zeros(W.shape)
+    new_W = W.copy()
+    
+    oneoversqrtlambda = 1/np.sqrt(l)
+    for t, example_id in enumerate(use_example_ids):
+        if t % 1000 == 0:
+            if verbose: print t
+        
+        y = Y[example_id]
+        scores = np.dot(W,X[example_id])
+        add_quantity = scores.max() - scores.min() + 1
+        true_class_best_score = scores[y]
+        not_class_best_score_id = np.argmax( scores - add_quantity * class_masks[y])
+        not_class_best_score = scores[not_class_best_score_id]
+        
+        scaling = 1./max(1,t+start_t)
+        new_W -= scaling * W
+        if (loss=='hinge' and true_class_best_score - not_class_best_score < 1) or (loss=='ramp' and abs(true_class_best_score - not_class_best_score) < 1):
+            eta = scaling/l
+            new_W[y] += eta*X[example_id]
+            new_W[not_class_best_score_id] -= eta*X[example_id]
+            
+        if do_projection:
+            if type(do_projection) == int:
+                if t % do_projection != 0: continue
+
+            W_norm = np.linalg.norm(new_W)
+            if W_norm == 0:
+                return (new_W, init_W + avg_W_update) if return_avg_W else new_W
+            scaling = oneoversqrtlambda/W_norm
+            new_W *= min(1,scaling)
+            
+        if return_avg_W:
+            avg_W_update += ((new_W - W) - avg_W_update)/(t+1)
+
+        # compute the zero-one loss to check for convergence
+        if loss_computation > 0 and (t % loss_computation == 0):
+            print "round %d: loss=%g" % (t, (np.dot(X,W.T).argmax(1) == Y).sum() / X.shape)
+
+        W[:] = new_W[:]
+
+    return (new_W, init_W + avg_W_update) if return_avg_W else new_W
+        
+        
+
+def multiclass_minibatch(Y,X,T,l,k,W,start_t=1,loss_computation=0,
+               return_avg_W,verbose=False,loss='hinge',
+               do_projection=False):
+    """
+    A multiclass implementation of the Pegasos
+    similar to the one presented by Zhuang Wang 2010
+
+    Parameters
+    ----------
+    Y :   (n_samples,)
+        Y classes
+    X :   (n_samples, n_features)
+        Data 
+    T :  int
+       Number of rounds over which to perform
+       optimization
+
+    l : float
+       lambda regularization parameter
+    k : int
+       batch size
+    W : (n_features,n_classifiers) 
+       Initializaiton for W -- required
+       the assumption is that W[:,c] is the
+       classifier for class c -- i.e. we work
+       from the columns which means that dot
+       products will work quickly with large
+       chunks of the data
+
+    return_avg_W : bool
+       If True then we return the W produced as the average
+       of the updates
+
+    start_t : int
+       Starting round -- in the case of a warm-start one
+       may not wish to begin with round 1 since, presumably
+       the first few rounds have already been passed
+
+    loss_computation : int
+       If 0 then we do not compute the loss over the data set while
+       running the algorithm (which is good if the dataset is large), 
+       otherwise if loss_computation > 0 we compute it every 
+       loss_computation rounds.
+
+    In each round we get the scores
+    then we construct a class-mask over the scores
+    and we map all the scores to a lower quantity
+    """
+    k = min(X.shape[0],k)
+    use_example_ids = np.random.randint(0,X.shape[0],size=(T,))
+    n_classes = max(W_classes[:,0].max()+1,Y.max()+1)
+    class_masks = np.zeros((n_classes, W_init
+
+def multiclass_weird_update(Y,X,T,l,k,W_init,W_classes,start_t=100,v=False,loss='hinge',
                do_projection=True,verbose=True):
     """
+    The update is declared weird since it does not actually use
+    the subgradient but another value
+
     Parameters
     ----------
     Y :   (n_samples,)
