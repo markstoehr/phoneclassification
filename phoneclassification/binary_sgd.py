8e6689a85fcd4a5b8246079385bfeec18482a14f
Mark Stoehr <stoehr.mark@gmail.com>
2014-02-18 08:28:30 -0600

    added code for tacking on an extra 1 to the end of the sparse representation

--- phoneclassification/binary_sgd.py
+++ phoneclassification/binary_sgd.py
@@ -24,3 +24,20 @@ def binary_to_bsparse(X):
     rowstartidx[1:] = np.cumsum(rownnz)
 
     return feature_ind, rownnz, rowstartidx
+
+def add_final_one(feature_ind, rownnz, rowstartidx,dim):
+    """
+    Need to be able to add a final 1 to the end of each feature vector
+    to account for the constant term
+    """
+    D = np.prod(dim)
+    new_feature_ind = np.zeros(len(feature_ind) + len(rownnz),dtype=np.intc)
+    new_rownnz = rownnz + 1
+    new_rowstartidx = np.arange(len(rowstartidx),dtype=np.intc) + rowstartidx
+    for i, rowidx in enumerate(rowstartidx[:-1]):
+        new_feature_ind[new_rowstartidx[i]:
+                        new_rowstartidx[i+1]-1] =feature_ind[rowidx:rowstartidx[i+1]]
+        assert new_rowstartidx[i+1] - new_rowstartidx[i] == new_rownnz[i]
+        new_feature_ind[new_rowstartidx[i+1]-1] = D
+
+    return new_feature_ind, new_rownnz, new_rowstartidx
